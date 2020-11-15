4e973bf3a2d51cdf6007a8820d6b40be010ee47a
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2020-11-14 23:15:32 -0600

    refactored to add buttons and random button

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
-import './Components/Card.css';
+import React from 'react';
+import './Card.css';
 
-class Card extends Component {
-   render(props) { 
-     return ( 
-       <div className="Card">
-         <h3>{this.props.title}</h3>
-         <p>{this.props.content}</p>
-       </div>
-       );
-   }
+export default function Card(props) {
+  return (
+    <div className='Card'>
+      <button type='button'onClick={() => props.onClickDelete(props.id)}>
+        delete
+      </button>
+      <h3>{props.title}</h3>
+      <p>{props.content}</p>
+    </div>
+  )
 }
- 
-export default Card;
+
+Card.propTypes = {
+  onClickDelete: () => {}
+}
